44157f3d1fe7a9b33d3e55bc975545db5889afc3
Joseph Yesselman <jyesselm@unl.edu>
2024-06-27 12:42:43 -0500

    updating setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ except ImportError:
     from distutils.core import setup
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
     sys.exit()
 
 
@@ -21,33 +21,31 @@ with open("requirements.txt", "r", encoding="UTF-8") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='rna_map_plotting',
-    version='0.1.0',
-    description='some common plotting to analyze DMS-MaPseq data',
+    name="rna_map_plotting",
+    version="0.1.0",
+    description="some common plotting to analyze DMS-MaPseq data",
     long_description=readme,
     long_description_content_type="test/markdown",
-    author='Joe Yesselman',
-    author_email='jyesselm@unl.edu',
-    url='https://github.com/jyesselm/rna_map_plotting',
+    author="Joe Yesselman",
+    author_email="jyesselm@unl.edu",
+    url="https://github.com/jyesselm/rna_map_plotting",
     packages=[
-        'rna_map_plotting',
+        "rna_map_plotting",
     ],
-    package_dir={'rna_map_plotting': 'rna_map_plotting'},
+    package_dir={"rna_map_plotting": "rna_map_plotting"},
     py_modules=[
-        'rna_map_plotting/cli'
+        "rna_map_plotting/logger",
+        "rna_map_plotting/plotting",
     ],
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
-    keywords='rna_map_plotting',
+    keywords="rna_map_plotting",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    entry_points = {
-        'console_scripts' : [
-        ]
-    }
+    entry_points={"console_scripts": []},
 )
